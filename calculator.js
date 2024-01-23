dd670304827a17f77a66a592d972a21795805b25
Kavipriya47 <155457844+Kavipriya47@users.noreply.github.com>
2024-01-23 15:10:27 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,19 +1,45 @@
-function appendToDisplay(value) {
-    document.calc.display.value += value;
-}
+document.addEventListener('DOMContentLoaded', function () {
+    const display = document.forms['calc']['display'];
 
-function clearDisplay() {
-    document.calc.display.value = '';
-}
-
-function backspace() {
-    document.calc.display.value = document.calc.display.value.slice(0, -1);
-}
-
-function calculate() {
-    try {
-        document.calc.display.value = eval(document.calc.display.value);
-    } catch (error) {
-        document.calc.display.value = 'Error';
+    function appendToDisplay(value) {
+        display.value += value;
     }
-}
+
+    function clearDisplay() {
+        display.value = '';
+    }
+
+    function backspace() {
+        display.value = display.value.slice(0, -1);
+    }
+
+    function calculate() {
+        try {
+            display.value = eval(display.value);
+        } catch (error) {
+            display.value = 'Error';
+        }
+    }
+    
+    const buttons = document.querySelectorAll('input[type="button"]');
+    buttons.forEach((button) => {
+        button.addEventListener('click', function () {
+            const buttonValue = this.value;
+
+            switch (buttonValue) {
+                case 'C':
+                    clearDisplay();
+                    break;
+                case '<-':
+                    backspace();
+                    break;
+                case '=':
+                    calculate();
+                    break;
+                default:
+                    appendToDisplay(buttonValue);
+                    break;
+            }
+        });
+    });
+});
